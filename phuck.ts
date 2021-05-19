aef94916b82a962526883bf868fae05d3dbe6f68
touchthescreen12 <jonathanago@ymail.com>
2021-05-18 23:56:15 -0600

    phuck.ts

---
+++ phuck.ts
@@ -0,0 +1,27 @@
+{
+   "name": "PHUCKS", #List name
+   "logoURI": "hhttps://givephuck.com/wp-content/uploads/2021/05/cropped-phuck-logo-1.png", #List logo uri
+   "timestamp": 1610339529000, #Update time in millisecond
+   "tokens": [{
+           "chainId": 1, #ChainId, which is currently 1
+           "address": "TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9", #Token address
+           "name": "PHUCKS", #Token name
+           "symbol": "PHUCK", #Token symbol
+           "decimals": 6, #Token decimals
+           "logoURI": "https://givephuck.com/wp-content/uploads/2021/05/cropped-phuck-logo-1.png" #Token logo uri
+       },
+       {
+           "chainId": 1,
+           "address": "TFUusM3Nxu63BZX95DsZZAhhwyftRkVjo6",
+           "name": "PHUCKS",
+           "symbol": "PHUCK",
+           "decimals": 18,
+           "logoURI": "https://givephuck.com/wp-content/uploads/2021/05/cropped-phuck-logo-1.png"
+       }
+   ],
+   "version": { #Version info
+       "major": 1, #Major version
+       "minor": 0, #Minor version
+       "patch": 0  #Patch version
+   }
+}
